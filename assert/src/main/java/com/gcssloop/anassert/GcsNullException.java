900bbb37d4cfc3ec2e8f20dc8f5530388bdc042f
GcsSloop <GcsSloop@gmail.com>
2017-04-26 01:39:45 +0800

    添加断言工具

---
+++ assert/src/main/java/com/gcssloop/anassert/GcsNullException.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2017 GcsSloop
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified 2017-04-26 01:32:57
+ *
+ * GitHub: https://github.com/GcsSloop
+ * WeiBo: http://weibo.com/GcsSloop
+ * WebSite: http://www.gcssloop.com
+ */
+
+package com.gcssloop.anassert;
+
+/**
+ * 空异常
+ */
+public class GcsNullException extends RuntimeException {
+
+    public GcsNullException() {
+    }
+
+    public GcsNullException(String message) {
+        super(message);
+    }
+
+    public GcsNullException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public GcsNullException(Throwable cause) {
+        super(cause);
+    }
+}
